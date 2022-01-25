256e77bfa22fef2a2dd57997fe11d809084abe47
Idris Raja <idris.raja@gmail.com>
2022-01-25 11:23:01 -0500

    wip

--- ex1-22.c
+++ ex1-22.c
@@ -2,74 +2,64 @@
 #include <stdlib.h>
 #include <assert.h>
 
-#define BLANK 0
-#define NOTBLANK 1
 #define MAXLINE 200
-#define COL 100
+#define COL 5
 
-int main() {
+int main(int argc, char *argv[]) {
     int c=0;
-    char buffer[MAXLINE];
-    int state=BLANK;
     int index=0;
-    int col=0;
+    int last_space=-1;
+    char buffer[COL];
 
     while ((c=getchar()) != EOF) {
-        if (state==BLANK) {
-            if (c=='\t' || c==' ') {
+        if (index<COL) {
+            if (c=='\n') {
                 buffer[index]=c;
                 index++;
-                col++;
-            } else if (c=='\n') {
-                index=0;
-                putchar(c);
-                col=0;
-            } else {
-                if (col>COL) {
-                    putchar('\n');
-                    col=0;
-                } else {
-                    for (int i=0; i<index; i++) {
-                        putchar(buffer[i]);
-                    }
+                for (int i=0; i<index; i++) {
+                    putchar(buffer[i]);
                 }
                 index=0;
-                state=NOTBLANK;
-                col++;
+                last_space=-1;
+            } else if (c==' ' || c=='\t') {
+                buffer[index]=c;
+                last_space=index;
+                index++;
+            } else {
                 buffer[index]=c;
                 index++;
             }
-        } else if (state==NOTBLANK) {
-            if (c=='\t' || c==' ') {
-                state=BLANK;
-                if (col>COL) {
-                    putchar('\n');
-                    col=0;
-                    for (int i=0; i<index; i++) {
-                        putchar(buffer[i]);
-                        col++;
-                    }
-                    index=0;
-                } else {
-                    for (int i=0; i<index; i++) {
-                        putchar(buffer[i]);
-                    }
-                    index=0;
-                    buffer[index]=c;
+        } else {
+            if (c==' ' || c=='\t') {
+                for (int i=0; i<index; i++) {
+                    putchar(buffer[i]);
+                }
+                index=0;
+                last_space=-1;
+                buffer[index]=c;
+                index++;
+            } else if (last_space==-1) {
+                for (int i=0; i<index-1; i++) {
+                    putchar(buffer[i]);
+                }
+                buffer[0]=buffer[index-1];
+                buffer[1]=c;
+                putchar('-');
+                index=2;
+            } else {
+                for (int i=0; i<last_space; i++) {
+                    putchar(buffer[i]);
+                }
+                index=0;
+                for (int i=last_space; i<COL; i++) {
+                    buffer[i-last_space]=buffer[i];
                     index++;
                 }
-            } else if (c=='\n') {
-                index=0;
-                putchar(c);
-                col=0;
-            } else {
-                buffer[index]=c;
-                index++;
-                col++;
+                buffer[0]=c;
+                index=1;
+                last_space=-1;
             }
-
-        } else {
-            exit(1);
+            putchar('\n');
         }
     }
 
